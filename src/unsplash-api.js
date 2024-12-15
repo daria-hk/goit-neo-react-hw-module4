e6a056fa2febdf8563904edc1447326470c7fe3f
daria_hk <honcharuk.daria95@gmail.com>
2024-12-15 16:56:39 +0100

    add files

--- src/unsplash-api.js
+++ src/unsplash-api.js
@@ -1,15 +1,10 @@
-// src/articles-api.js
 import axios from "axios";
 
 const API_KEY = "nmMQbO2J997t5bzDRKDSMh3l1djlqzWLPyO2ggAd9SQ";
 
 axios.defaults.baseURL = "https://api.unsplash.com/";
 
-export const fetchPhotosForGallery = async ({
-  query,
-  page = 1,
-  per_page = 10,
-}) => {
+export const fetchPhotosForGallery = async ({ query, page, per_page }) => {
   try {
     const res = await axios.get("search/photos", {
       params: {
@@ -20,7 +15,6 @@ export const fetchPhotosForGallery = async ({
       },
     });
 
-    console.log(res.data.results);
     return res.data.results;
   } catch (error) {
     console.error("Error in API request:", error);
